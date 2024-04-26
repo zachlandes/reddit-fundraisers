ef23a8daa6987c299d68853493cc7667a211a33b
zachlandes <zlandes@gmail.com>
2024-04-25 17:08:34 -0700

    Draft of chaining forms for creating fundraiser post
    * Added custom post type with entry point form for selecting a nonprofit
    to donate to
    * Added a function that goes to every.org to search for nonprofits
    * Need to get nonprofitresults to be passed to select form

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // Learn more at developers.reddit.com/docs
-import { Devvit } from '@devvit/public-api';
+import { Context, Devvit, SettingsClient } from '@devvit/public-api';
 
 Devvit.configure({
   redditAPI: true,
@@ -10,7 +10,7 @@ Devvit.configure({
 Devvit.addMenuItem({
   label: 'Add my custom post',
   location: 'subreddit',
-  forUserType: 'moderator',
+  forUserType: 'member',
   onPress: async (_event, context) => {
     const { reddit, ui } = context;
     const subreddit = await reddit.getCurrentSubreddit();
@@ -28,32 +28,100 @@ Devvit.addMenuItem({
   },
 });
 
-// Add a custom post type definition
+// const App: Devvit.CustomPostComponent = async (context) => {
+
+// };
+async function fetchNonProfitResults(query: string, settings: SettingsClient): Promise<string[]> {
+  const everyPublicKey = await settings.get('every-public-api-key');
+  let response = await fetch(`https://partners.every.org/v0.2/search/${query}?apiKey=${everyPublicKey}`);
+  if (!response.ok) {
+    throw new Error('Every.org search request failed');
+  }
+  const data = await response.json();
+  return data.nonprofits.map(item: object => item.name);
+}
+
 Devvit.addCustomPostType({
-  name: 'Custom Post',
-  height: 'regular',
-  render: (context) => {
-    const { useState } = context;
-    const [counter, setCounter] = useState(0);
+  name: 'Multi-step form',
+  description: 'Create and post a fundraiser',
+  render: ({ useForm, useState, ui, settings}) => {
+    const [name, setName] = useState('Mysterious person');
+    let nonProfitResults: string[] = []; 
+	// Form must be defined within the render method
+    const searchForm = useForm(
+      () => ({
+        title: `${name[0]}, which nonprofit do you want to fundraise for?`,
+        fields: [
+          {
+            type: 'string',
+            label: 'Search for a nonprofit',
+            name: 'search',
+          },
+        ],
+      }),
+      ({values}) => {
+        const search = values['search'][0]
+        if (search.length > 0) {
+          nonProfitResults = await fetchNonProfitResults(search, settings)
+          ui.showForm(searchResultsForm);
+        }
+      }
+    );
+
+    const searchResultsForm = useForm(
+      () => ({
+        title: "results",
+        fields: [
+          {
+          type: 'select',
+          label: 'Nonprofit search results'
+          name: 'nonprofit',
+          options: nonProfitResults
+        }
+      ]
+    }),
+      ({values}) => {
+        
+      }
+    )
 
     return (
-      <vstack height="100%" width="100%" gap="medium" alignment="center middle">
-        <image
-          url="logo.png"
-          description="logo"
-          imageHeight={256}
-          imageWidth={256}
-          height="48px"
-          width="48px"
-        />
-        <text size="large">{`Click counter: ${counter}`}</text>
-        <button appearance="primary" onPress={() => setCounter((counter) => counter + 1)}>
-          Click me!
-        </button>
+      <vstack>
+        <text>Hello {name}</text>
+
+	// Add a button which calls ui.showForm();
+        <button onPress={() => { ui.showForm(searchForm) }}>Change name</button>
       </vstack>
-    );
-  },
+    )
+  }
 });
+
+// // Add a custom post type definition
+// Devvit.addCustomPostType({
+//   name: 'Custom Post',
+//   height: 'regular',
+//   render: (context) => {
+//     const { useState } = context;
+//     const [counter, startDonation] = useState(0);
+
+//     return (
+//       <vstack height="100%" width="100%" gap="medium" alignment="center middle">
+//         <image
+//           url="donate.png"
+//           description="logo"
+//           imageHeight={51}
+//           imageWidth={138}
+//           height="48px"
+//           width="48px"
+//         />
+//         <text size="large">{`Click counter: ${counter}`}</text>
+//         <button appearance="primary" onPress={() => context.ui.navigateTo('https://www.every.org/unrsf/f/help-halve-road-deaths')}>
+//           Click me!
+//         </button>
+//       </vstack>
+//     );
+//   },
+// });
 
 Devvit.addSettings([
   {
